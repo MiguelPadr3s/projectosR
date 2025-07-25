2a27f534e501e06a0bb87d44a75ef2c9a97a21aa
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-25 16:56:21 -0700

    star rating

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component*/}
       {/* <Accordian/> */}
+
       {/* random colors */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+
+      {/* star rating component */}
+      <StarRating noOfStars={10}/>
+
     </div>
 
   );
