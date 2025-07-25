8df99b0d1bc0265ec565b427f84cbeb81491bb12
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-24 17:51:47 -0700

    styles

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,21 +3,32 @@
 
 import { useState } from "react";
 import data from "./data";
+import './styles.css';
 
-export default function Accordian() {
 
+export default function Accordian(getCurrentId) {
         const [selected, setSelected] = useState(null);
-    
+        function handleSingleSelection (getCurrentId){
+            setSelected(getCurrentId === selected ? null : getCurrentId);
+        }
+
+    console.log(selected);
     return (
         <div className="wrapper">
+            <button>Enable Multi Selection</button>
             <div className="accordian">
                 {data && data.length > 0 ? (
                     data.map((dataItem) => (
                         <div className="item">
-                            <div className="title">
+                            <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
                             </div>
+                            {
+                                selected === dataItem.id ? 
+                                <div className="content">{dataItem.answer}</div>
+                                : null
+                            }
                         </div>
                     ))
                 ) : (
@@ -26,5 +37,5 @@ export default function Accordian() {
             </div>
         </div>
     );
-    
+
 }
