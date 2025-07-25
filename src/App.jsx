530860929bd8b0ded6ee1d4ffdc07fc26fa8fea2
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-25 15:22:44 -0700

    Random Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component*/}
-      <Accordian/>
+      {/* <Accordian/> */}
+      {/* random colors */}
+      <RandomColor/>
     </div>
 
   );
