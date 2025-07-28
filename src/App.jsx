0cd8acfaf7a2df20944cb3fe170a1109a838e5a4
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-28 12:52:05 -0700

    Load more button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -14,7 +15,10 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* star rating component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Loading more data */}
+      <LoadMoreData/>
 
     </div>
 
