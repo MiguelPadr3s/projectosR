333f614e7be4efe4ecd70b56cfdfa113fc5a8736
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-07-29 09:32:38 -0700

    weather

--- src/components/Weather-folder/search/index.jsx
+++ src/components/Weather-folder/search/index.jsx
@@ -1,16 +1,18 @@
-import "./styles.css"
+import "./styles.css";
 
-
-export default function Search({search, setSearch}){
-    return <div className="search-engine">
-        <input
+export default function Search({ search, setSearch, handleSearch }) {
+  return (
+    <div className="search-engine">
+      <input
         type="text"
-        className="city-search"
         placeholder="Enter City Name"
         name="search"
         value={search}
-        onChange={(event)=> setSearch(event.target.value)}
-        />
-        <button className="search-btn">Search Weather</button>
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button onClick={handleSearch}>
+        Search
+      </button>
     </div>
+  );
 }
